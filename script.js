7698293e27b0da2cfb5c44fae408261fa6a975d5
Dmitry Shamko <proffery@gmail.com>
2023-03-06 11:30:54 +0300

    render function

--- script.js
+++ script.js
@@ -16,37 +16,13 @@ function Book(title, authorName, pages, readed) {
 }
 
 function addBookToLibrary(e) {
-    if (formTitle.value.length < 3 || formAuthor.value.length < 3) {
+    if (formTitle.value.length < 3 || formAuthor.value.length < 3 || formPages.value % 1 != 0 || formPages.value < 1) {
         console.log('INPUT ERROR');
     }
     else {
         const newBook = new Book(formTitle.value, formAuthor.value, formPages.value, formReaded.checked);
         myLibrarry.push(newBook);
-
-        let bookCard = document.createElement('div');
-        bookCard.id = (`${myLibrarry.length - 1}`);
-        if (myLibrarry[myLibrarry.length - 1].readed) {
-            bookCard.classList.add('book-card-readed');
-        }
-        else {
-            bookCard.classList.add('book-card-unreaded');
-        }
-        library.appendChild(bookCard);
-        let title = document.createElement('div');
-        title.textContent = `Book: ${myLibrarry[myLibrarry.length - 1].title}`;
-        bookCard.appendChild(title);
-        let authorName = document.createElement('div');
-        authorName.textContent = `Author: ${myLibrarry[myLibrarry.length - 1].authorName}`;
-        bookCard.appendChild(authorName);
-        let pages = document.createElement('div');
-        pages.textContent = `Pages: ${myLibrarry[myLibrarry.length - 1].pages}`;
-        bookCard.appendChild(pages);
-        let remove = document.createElement('button');
-        remove.classList.add('remove');
-        remove.id = (`${myLibrarry.length - 1}`);
-        remove.textContent = 'Remove';
-        bookCard.appendChild(remove);
-        remove.addEventListener('click', removeBook);
+        renderLibrary();
     }
     console.log(myLibrarry);
     removeButtons = document.querySelectorAll('.remove');
@@ -54,10 +30,61 @@ function addBookToLibrary(e) {
     e.preventDefault();
 }
 
+function renderLibrary () {
+    const allBooks = document.querySelectorAll('.card')
+    allBooks.forEach(book => book.remove());
+    for (let i = 0; i < myLibrarry.length; i++) {
+        let bookCard = document.createElement('div');
+        bookCard.id = (`${[i]}`);
+        bookCard.classList.add('card');
+        library.appendChild(bookCard);
+
+        let title = document.createElement('div');
+        title.textContent = `Book: ${myLibrarry[i].title}`;
+        bookCard.appendChild(title);
+
+        let authorName = document.createElement('div');
+        authorName.textContent = `Author: ${myLibrarry[i].authorName}`;
+        bookCard.appendChild(authorName);
+
+        let pages = document.createElement('div');
+        pages.textContent = `Pages: ${myLibrarry[i].pages}`;
+        bookCard.appendChild(pages);
+
+        let statusDiv = document.createElement('div');
+        statusDiv.classList.add('status-container');
+        bookCard.appendChild(statusDiv);
+
+        let statusLabel = document.createElement('label');
+        statusLabel.setAttribute('for', `readed${i}`);
+        statusLabel.textContent = 'Readed:';
+        statusDiv.appendChild(statusLabel);
+
+        let status = document.createElement('input');
+        status.id = (`readed${i}`)
+        status.setAttribute('type', "checkbox");
+        if (myLibrarry[i].readed) {
+            bookCard.classList.add('book-card-readed');
+            status.setAttribute('checked', '');
+        }
+        else {
+            bookCard.classList.add('book-card-unreaded');
+        }
+        statusDiv.appendChild(status);
+
+        let remove = document.createElement('button');
+        remove.classList.add('remove');
+        remove.id = (`${i}`);
+        remove.textContent = 'Remove';
+        bookCard.appendChild(remove);
+        
+        remove.addEventListener('click', removeBook);
+    }
+}
+
 function removeBook(e) {
-    const removeDiv = document.querySelector(`div[id="${e.target.id}"]`);
-    library.removeChild(removeDiv);
     myLibrarry.splice(e.target.id, 1);
+    renderLibrary();
     removeButtons = document.querySelectorAll('.remove');
     console.log(myLibrarry);
     console.log(removeButtons);
